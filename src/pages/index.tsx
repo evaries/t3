3c1d3413b86470b52f8a778267a668713b2fa6fa
kuna <kuna@MacBook-Pro-kuna.local>
2023-06-19 08:34:26 +0200

    fixed non-auth call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ import { api } from "y/utils/api";
 const Home: NextPage = () => {
   const { isSignedIn, user } = useUser();
   const { push } = useRouter();
+
+  if (!isSignedIn)
+    return (
+      <main className="flex h-full w-full items-center justify-center">
+        <div>Here will be landing page for non auth state</div>;
+      </main>
+    );
+
   const { data } = api.user.getCurrentUser.useQuery();
 
   return (
@@ -18,34 +26,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-full w-full items-center justify-center">
-        {/*TODO:separate this into components*/}
-        {!isSignedIn ? (
-          <div>Here will be landing page for non auth state</div>
-        ) : (
-          <div className="flex flex-col items-center ">
-            <div>Hello {user.firstName}! Looks great today!</div>
-            <div className="mt-3 flex ">
-              <button
-                onClick={(e) => {
-                  void e.preventDefault();
-                  void push("/dashboard");
-                }}
-                className="mx-2 rounded-md	 bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
-              >
-                go to dashboard
-              </button>
-              <button
-                onClick={(e) => {
-                  void e.preventDefault();
-                  void push(data ? `/${data?.username}` : "");
-                }}
-                className="mx-2 rounded-md	 bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
-              >
-                see public page
-              </button>
-            </div>
+        <div className="flex flex-col items-center ">
+          <div>Hello {user.firstName}! Looks great today!</div>
+          <div className="mt-3 flex ">
+            <button
+              onClick={(e) => {
+                void e.preventDefault();
+                void push("/dashboard");
+              }}
+              className="mx-2 rounded-md	 bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
+            >
+              go to dashboard
+            </button>
+            <button
+              onClick={(e) => {
+                void e.preventDefault();
+                void push(data ? `/${data?.username}` : "");
+              }}
+              className="mx-2 rounded-md	 bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
+            >
+              see public page
+            </button>
           </div>
-        )}
+        </div>
       </main>
     </>
   );
