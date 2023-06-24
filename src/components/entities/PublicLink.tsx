84fc191fd0abd353f6ed60119d7cb98784a964db
kuna <kuna@MacBook-Pro-kuna.local>
2023-06-24 13:37:40 +0200

    added toggle handler, layout for share icons

--- src/components/entities/PublicLink.tsx
+++ src/components/entities/PublicLink.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import ShareIconLinks from "../shared/ShareIconLinks";
 
 export type PublicLinkProps = {
   name: string;
@@ -10,8 +11,18 @@ const PublicLink: NextPage<PublicLinkProps> = ({ name, href }) => {
   return (
     <div className="centered h-30 w-full max-w-xs">
       <Link href={href} target="_blank" passHref={true} className="w-full">
-        <div className="mt-2 flex inline-flex w-80 w-full items-center justify-center rounded-lg bg-gray-200 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-300 focus:outline-none dark:focus:ring-gray-500">
+        <div className="relative mt-2 inline-flex w-80 w-full items-center justify-center rounded-lg bg-gray-200 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-300 focus:outline-none dark:focus:ring-gray-500">
           {name}
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              console.log("copied", href);
+            }}
+            className="absolute right-5 "
+          >
+            <ShareIconLinks />
+          </div>
         </div>
       </Link>
     </div>
